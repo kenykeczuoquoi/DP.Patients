c7cb7e3ef68cc26a98aa59666b573ed465752ff8
kenykeczuoquoi <72889148+kenykeczuoquoi@users.noreply.github.com>
2020-10-14 21:54:26 +0200

    Add files via upload

---
+++ DP.Patients/DP.Patients/Model/Patient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DP.Patients.Model
+{
+    public class Patient
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public DateTime PositiveTestDate { get; set; }
+    }
+}
